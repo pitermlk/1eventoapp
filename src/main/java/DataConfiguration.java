e96994b153e9f76aed65b19609092587969482ad
INDRA\placerda <placerda@minsait.com>
2022-02-22 20:55:01 -0300

    configurePostGresSQL

--- src/main/java/DataConfiguration.java
+++ src/main/java/DataConfiguration.java
@@ -1,34 +1,53 @@
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Profile("dev")
 @Configuration
 public class DataConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-        dataSource.setUsername("root");
-        dataSource.setPassword("Xmlk8294x");
-        return dataSource;
+
+    @Configuration
+    public class MainConfig {
+
+        @Bean
+        public BasicDataSource dataSource() throws URISyntaxException {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            BasicDataSource basicDataSource = new BasicDataSource();
+            basicDataSource.setUrl(dbUrl);
+            basicDataSource.setUsername(username);
+            basicDataSource.setPassword(password);
+
+            return basicDataSource;
+        }
     }
 
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
-        adapter.setPrepareConnection(true);
-        return adapter;
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("Xmlk8294x");
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public JpaVendorAdapter jpaVendorAdapter(){
+//        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//        adapter.setDatabase(Database.MYSQL);
+//        adapter.setShowSql(true);
+//        adapter.setGenerateDdl(true);
+//        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+//        adapter.setPrepareConnection(true);
+//        return adapter;
+//    }
 }
